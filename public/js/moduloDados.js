25b24cba851231db81a07065ce901654a29bdb04
sandrocolucci <alexsandro.colucci@etec.sp.gov.br>
2023-11-21 20:42:49 -0300

    aula211modulos em Java Script

---
+++ public/js/moduloDados.js
@@ -0,0 +1,6 @@
+const userLogin=[
+    {id:1001, username:"sandroColucci", senha:"@Cadeado250", email: "alexsandro.colucci@etec.sp.gov.br"},
+    {id:1002, username:"pedro", senha:"@cvd36541", email: "pedro@gmail.com"},
+
+]
+export default userLogin
