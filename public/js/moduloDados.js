ab9b795e7e6ef3f345abd2dae0865a5e2aca7c26
sandrocolucci <alexsandro.colucci@etec.sp.gov.br>
2023-11-28 20:52:50 -0300

    2811

--- public/js/moduloDados.js
+++ public/js/moduloDados.js
@@ -1,6 +1,7 @@
 const userLogin=[
     {id:1001, username:"sandroColucci", senha:"@Cadeado250", email: "alexsandro.colucci@etec.sp.gov.br"},
     {id:1002, username:"pedro", senha:"@cvd36541", email: "pedro@gmail.com"},
+    {id:1003, username:"celsopadaria", senha:"totoro16", email: "celsodapadaria@gmail.com"}
 
 ]
 export default userLogin
